fe49f270fdc53e5715e4372c4801ff7acaca0bbd
Dohyung Lee <tv2464@naver.com>
2024-01-30 23:53:35 +0900

    20240130 세미콜론, password 암호화

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 8081
+const express = require('express');
+const app = express();
+const port = 8081;
 const bodyParser = require('body-parser');
 const {User} = require("./models/User");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
-const config = require("./config/key")
+const mongoose = require('mongoose');
+const config = require("./config/key");
 
 mongoose.connect(config.mongoURI).then(() => console.log("Monggo DB Conected..."))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~')
-})
+  res.send('Hello World!~');
+});
 
 app.post('/register', async(req,res) => {
   // 회원 가입 시 필요한 정보를 받고 DB에 저장 처리
 
-  const user = new User(req.body)
-  console.log(req.body);
+  const user = new User(req.body);
 
   // mongo DB 메소드
   // https://www.inflearn.com/questions/805491 참고
   const result = await user.save().then(()=>{
     return res.status(200).json({
       success: true
-    })
+    });
   }).catch((err) =>{
     console.log(err);
-  })
-})
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
--- models/User.js
+++ models/User.js
@@ -29,7 +29,27 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
-})
+});
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if (user.isModified('password')) {
+        //비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err);
+
+                user.password = hash;
+                //console.log("passward encrypt : " + user.password);
+                next();
+            });
+        });
+    }
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = {User};
